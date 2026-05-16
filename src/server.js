065370f3cbf055e142d4dfb45b994e0d4837821b
veronika <sustv153@gmail.com>
2026-05-16 17:51:31 +0300

    fix routers

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { errors } from 'celebrate';
 
-import notesRouter from './src/routes/notesRoutes.js';
+import notesRouter from './routes/notesRoutes.js';
 
 dotenv.config();
 
