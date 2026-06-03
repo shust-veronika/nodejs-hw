9e01199aef52d2c7b4ae0208e500175b65c4c74a
veronika <sustv153@gmail.com>
2026-06-03 13:56:08 +0300

    git

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,11 @@ const userSchema = new Schema(
       required: true,
       minlength: 8,
     },
+    avatar: {
+  type: String,
+  default:
+    'https://ac.goit.global/fullstack/react/default-avatar.jpg',
+},
   },
   {
     timestamps: true,
@@ -30,10 +35,7 @@ userSchema.methods.toJSON = function () {
 };
 
 userSchema.pre('save', function (next) {
-  if (!this.username) {
-    this.username = this.email;
-  }
-
+  this.username = this.email;
   next();
 });
 
